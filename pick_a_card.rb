52cb6f9832db87b91a91feca08f6361cee7bab86
faks08 <andrej.909090.pe@gmail.com>
2022-02-08 16:38:22 +0700

    Unicode suit symbols

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,5 +1,15 @@
 #encoding UTF-8
-values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades']
+#
+if Gem.win_platform?
+  Encoding.default_external = Encoding.find(Encoding.locale_charmap)
+  Encoding.default_internal = __ENCODING__
 
-puts "#[values.sample of #[suits.sample]"
+  [STDIN, STDOUT].each do |io|
+    io.set_encoding(Encoding.default_external, Encoding.default_internal)
+  end
+end
+#
+values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+suits = ['?', '?', '? ', '?']
+
+puts values.sample + suits.sample
